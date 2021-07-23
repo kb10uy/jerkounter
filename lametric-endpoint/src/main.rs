422ac2dd02f449619170e193677b9d39fb5ed069
Yū Kobayashi <kb10uy@kb10uy.org>
2021-07-23 23:17:05 +0900

    Refactor

--- lametric-endpoint/src/main.rs
+++ lametric-endpoint/src/main.rs
@@ -5,7 +5,7 @@ use lametric::{LaMetricFrame, LaMetricResponse, LM_ICON_CLOCK, LM_ICON_SPERM, LM
 use schema::AppParameters;
 
 use anyhow::Result;
-use chrono::prelude::*;
+use chrono::{prelude::*, Duration};
 use log::info;
 use once_cell::sync::Lazy;
 use regex::Regex;
@@ -15,14 +15,14 @@ use tissue_rs::{CheckinBuilder, CheckinResponse, IncomingEndpoint};
 
 const USER_AGENT: &str = concat!("Jerkounter/LaMetric ", env!("CARGO_PKG_VERSION"));
 static RE_CHECKINS: Lazy<Regex> =
-    Lazy::new(|| Regex::new(r#">\s*通算回数\s*:\s*([\d,]+)\s*回\s*</"#).expect("Invalid regex"));
-static RE_INTERVAL: Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r#">\s*([\d]+)\s*日\s*([\d]+)\s*時間\s*([\d]+)\s*分経過</"#).expect("Invalid regex")
-});
+    Lazy::new(|| Regex::new(r#"通算回数:\s*([\d,]+)\s*回"#).expect("Invalid regex"));
+static RE_INTERVAL: Lazy<Regex> =
+    Lazy::new(|| Regex::new(r#"([\d]+)日\s*([\d]+)時間\s*([\d]+)分経過"#).expect("Invalid regex"));
 
 #[async_std::main]
 async fn main() -> Result<()> {
     pretty_env_logger::init();
+    info!("User-Agent: {}", USER_AGENT);
 
     let mut app = tide::new();
     app.at("/user").get(fetch_user);
@@ -53,7 +53,12 @@ async fn fetch_user(request: Request<()>) -> TideResult {
             LaMetricFrame {
                 index: 2,
                 icon: Some(LM_ICON_CLOCK),
-                text: format!("{} 日 {} 時間 {} 分", interval.0, interval.1, interval.2),
+                text: format!(
+                    "{} 日 {} 時間 {} 分",
+                    interval.num_days(),
+                    interval.num_hours(),
+                    interval.num_minutes()
+                ),
             },
         ],
     };
@@ -79,7 +84,7 @@ async fn send_checkin(request: Request<()>) -> TideResult {
 }
 
 /// Fetches information from Tissue.
-async fn fetch_user_info(username: &str) -> Result<(i32, (i32, i32, i32)), TideError> {
+async fn fetch_user_info(username: &str) -> Result<(usize, Duration), TideError> {
     let url = format!("https://shikorism.net/user/{}", username);
     let user_html = surf::get(url)
         .header("User-Agent", USER_AGENT)
@@ -91,17 +96,18 @@ async fn fetch_user_info(username: &str) -> Result<(i32, (i32, i32, i32)), TideE
         RE_CHECKINS.captures(&user_html),
         RE_INTERVAL.captures(&user_html),
     ) {
-        (Some(cm), Some(im)) => {
-            let c = cm
+        (Some(cmatch), Some(imatch)) => {
+            let checkins = cmatch
                 .get(1)
                 .map_or("0", |m| m.as_str())
                 .replace(',', "")
-                .parse::<i32>()?;
-            let id = im.get(1).map_or("0", |m| m.as_str()).parse::<i32>()?;
-            let ih = im.get(2).map_or("0", |m| m.as_str()).parse::<i32>()?;
-            let im = im.get(3).map_or("0", |m| m.as_str()).parse::<i32>()?;
-            Ok((c, (id, ih, im)))
+                .parse()?;
+            let interval = Duration::days(imatch.get(1).map_or("0", |m| m.as_str()).parse()?)
+                + Duration::hours(imatch.get(2).map_or("0", |m| m.as_str()).parse()?)
+                + Duration::minutes(imatch.get(3).map_or("0", |m| m.as_str()).parse()?);
+
+            Ok((checkins, interval))
         }
-        _ => Ok((0, (0, 0, 0))),
+        _ => Ok((0, Duration::zero())),
     }
 }
